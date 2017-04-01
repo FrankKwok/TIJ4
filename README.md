a0233cf275322391a75b415b5be7ac0e153bb9c3
Frank Kwok <ghlfrankkwok@gmail.com>
2017-04-01 14:33:41 +0800

    Create Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TIJ4
 Thinking In Java 4th Edition
+
+This project does not include the GUI part
---
+++ src/main/java/com/github/frankkwok/tij4/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Thinking In Java 4th Edition
+ *
+ * @author Frank Kwok on 2017/4/1.
+ */
+package com.github.frankkwok.tij4;
