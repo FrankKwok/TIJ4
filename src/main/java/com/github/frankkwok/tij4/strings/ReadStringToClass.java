ee2a7886c9be7ffd44d1a2f76de5384acdafa4c6
Frank Kwok <ghlfrankkwok@gmail.com>
2017-05-15 18:22:23 +0800

    Scanner

--- src/main/java/com/github/frankkwok/tij4/strings/ReadStringToClass.java
+++ src/main/java/com/github/frankkwok/tij4/strings/ReadStringToClass.java
@@ -1,5 +1,7 @@
 package com.github.frankkwok.tij4.strings;
 
+import java.util.Scanner;
+
 /**
  * Page 410
  * Exercise 20: Create a class that contains int, long, float and double and String fields. Create a constructor for
@@ -9,4 +11,34 @@ package com.github.frankkwok.tij4.strings;
  * @author Frank Kwok on 2017/5/9.
  */
 public class ReadStringToClass {
+    int i;
+    long l;
+    float f;
+    double d;
+    String s;
+
+    public ReadStringToClass(String text) {
+        Scanner scanner = new Scanner(text);
+        i = scanner.nextInt();
+        l = scanner.nextLong();
+        f = scanner.nextFloat();
+        d = scanner.nextDouble();
+        s = scanner.next();
+    }
+
+    public static void main(String[] args) {
+        ReadStringToClass r = new ReadStringToClass("1 2 3.0 4.0 frank");
+        System.out.println(r);
+    }
+
+    @Override
+    public String toString() {
+        return "ReadStringToClass{" +
+                "i=" + i +
+                ", l=" + l +
+                ", f=" + f +
+                ", d=" + d +
+                ", s='" + s + '\'' +
+                '}';
+    }
 }
